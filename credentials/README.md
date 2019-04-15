a24292db42cf7e7124f28370e37132928ce64848
alldias <alexandre@fabsoft.com.br>
2019-04-14 22:33:51 -0300

    feat: add watson natural language understanding service

--- credentials/README.md
+++ credentials/README.md
@@ -8,3 +8,18 @@ File: `algorithmia.json`
     "apiKey" : "simOXejIusnLaZQnm7tKl27n+ZD1"
 }
 ```
+
+
+## Watson Natural Language Understanding
+
+File: `watson-nlu.json`
+```
+{
+  "apikey": "zdfEVdD65hGakag7BGP-3efQ7IA9SMc6ffhvnV3m60FA",
+  "iam_apikey_description": "Auto generated apikey during resource-key operation for Instance - crn:v1:bluemix:public:natural-language-understanding:us-south:a/45459079fb4f4971bd376eec2464193a:1f4757bb-99bf-42c1-b57f-d0aab437c405::",
+  "iam_apikey_name": "auto-generated-apikey-770eef5a-bf0c-4525-bd4e-47f0efff6fd0",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/45459079fb4f4971bd376eec2464193a::serviceid:ServiceId-64eaaed2-4959-48ac-86c6-e66ae2d44b96",
+  "url": "https://gateway.watsonplatform.net/natural-language-understanding/api"
+}
+```
--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,30 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBounderyDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apiKey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+
+var nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    username: 'alexandre@fabsoft.com.br',
+    password: '@Lm20mds07',
+    version: '2018-11-16',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
+})
+
+/*
+    username: 'alexandre@fabsoft.com.br',
+    password: '@Lm20mds07',
+    version: '2018-04-05',
+
+*/ 
+
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
-    breaContentIntoSentences(content)
+    breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content){
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -35,7 +55,7 @@ async function robot(content) {
             return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm,'').replace(/ /g,' ')
         }
     }
-    function breaContentIntoSentences(content){
+    function breakContentIntoSentences(content){
         content.sentences = []
 
         const sentences = sentenceBounderyDetection.sentences(content.sourceContentSanitized)
@@ -46,6 +66,39 @@ async function robot(content) {
                 images:[]
             })
         })
+    }
+
+    async function fetchKeywordsOfAllSentences(content){
+        for (const sentence of content.sentences){
+            sentence.keyword = await fetchWatsonAndReturnKeywords(sentence.text)
+        }        
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence){
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if(error) {
+                    // throw error
+                    console.log(error);
+                    return;
+                }
+
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                })
+    
+                resolve(keywords)
+            })
+        })
+    }    
+
+    function limitMaximumSentences(content){
+        content.sentences = content.sentences.slice(0,content.maximumSentences)
     }
 }
 
